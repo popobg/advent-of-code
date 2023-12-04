f37239808987c62bb805b2ba81d71587a2f70be5
popobg <pauline.bouyssou1@gmail.com>
2023-12-04 16:13:32 +0100

    add functions and comments to first day challenge

--- 2023/day_1/trebuchet.py
+++ 2023/day_1/trebuchet.py
@@ -3,6 +3,32 @@
 """We are searching for the sum of all the calibration values.
 The list has been messed up and the calibration values are the first and the last digit for each line. Be careful, the numbers can be spelled out with letters."""
 
+# FUNCTIONS
+
+def catching_number(line: str, calibration_line: list, x: int):
+    """catch the first or the last number of the string whether x = 1 or x = -1.
+    Detect spelled out number too."""
+    new_line = ""
+    for i in line[::x]:
+        try:
+            int(i)
+            calibration_line.append(i)
+            return calibration_line
+        except:
+            new_line += i
+            if len(new_line) >= 3:
+                for n in spelled_numbers.keys():
+                    if n in new_line[::x]:
+                        calibration_line.append(str(spelled_numbers[n]))
+                        return calibration_line
+
+def calculate_sum(sum, line_values):
+    """add the calibration value of the line to the sum"""
+    sum += line_values
+    return sum
+
+# GLOBAL VARIABLES
+
 spelled_numbers = {
     "one": 1,
     "two": 2,
@@ -15,48 +41,25 @@ spelled_numbers = {
     "nine": 9
 }
 
-with open("calibration_doc.txt", "r") as f :
-    calibration_values = []
-    for line in f:
-        calibration_line = []
-        # searching for the first digit of the string
-        new_line = ""
-        for i in line:
-            if len(calibration_line) == 1:
-                break
-            else:
-                try:
-                    int(i)
-                    calibration_line.append(i)
-                    break
-                except:
-                    new_line += i
-                    if len(new_line) >= 3:
-                        for n in spelled_numbers.keys():
-                            if n in new_line:
-                                calibration_line.append(str(spelled_numbers[n]))
-                                break
-        # searching for the last digit of the string
-        new_line = ""
-        for i in line[::-1]:
-            if len(calibration_line) == 2:
-                break
-            else:
-                try:
-                    int(i)
-                    calibration_line.append(i)
-                    break
-                except:
-                    new_line += i
-                    if len(new_line) >= 3 :
-                        for n in spelled_numbers.keys():
-                            if n in new_line[::-1]:
-                                calibration_line.append(str(spelled_numbers[n]))
-                                break
-        calibration_values.append("".join(calibration_line))
+calibration_sum = 0
 
-sum = 0
-for i in calibration_values:
-    sum += int(i)
+# MAIN
 
-print(sum)
+f = open("calibration_doc.txt", "r", encoding= "utf-8")
+
+for line in f:
+    line = line.strip()
+    calibration_line = []
+
+    # searching for the first digit of the string
+    calibration_line = catching_number(line, calibration_line, 1)
+
+    # searching for the last digit of the string
+    calibration_line = catching_number(line, calibration_line, -1)
+
+    calibration_line = int("".join(calibration_line))
+    calibration_sum = calculate_sum(calibration_sum, calibration_line)
+
+print(calibration_sum)
+
+f.close()
